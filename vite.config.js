1baad524914ef1cba8710930785d2edcb48a76f4
umanijaz <umanijaz5@gmail.com>
2025-08-13 15:40:01 +0500

    feat: add fonts to the project not-used-yet

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: ["d11d11e56032.ngrok-free.app"],
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
   },
 });
